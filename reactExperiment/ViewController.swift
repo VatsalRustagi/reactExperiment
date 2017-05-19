845cacfe63f5e3d52b26e51a81f17be37ed147cd
Vatsal Rustagi <vatsalrustagi@Vatsals-MacBook-Pro.local>
2017-05-19 15:31:38 -0700

    First Commit

---
+++ reactExperiment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reactExperiment
+//
+//  Created by Vatsal Rustagi on 5/19/17.
+//  Copyright © 2017 Vatsalr23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
